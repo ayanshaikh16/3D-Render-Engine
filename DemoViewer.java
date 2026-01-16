4cb94d21f11ec6a84269cd496eeb2363fc4a34e4
ayanshaikh16 <ashaikh91696@gmail.com>
2026-01-16 17:45:13 -0500

    Update DemoViewer.java

--- DemoViewer.java
+++ DemoViewer.java
@@ -47,10 +47,16 @@ public class DemoViewer{
                 });
                 Matrix3 transform = headingTransform.multiply(pitchTransform);
 
-                g2.translate(getWidth() / 2, getHeight() / 2);
+                
                 g2.setColor(Color.WHITE);
 
-                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB)
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                for(int q = 0; q < zBuffer.length; q++){
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+
                 for (Triangle t : tris) {
 
                     Vertex v1 = transform.transform(t.v1);
@@ -67,15 +73,33 @@ public class DemoViewer{
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
                     int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
 
-                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+                    
 
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.closePath();
-                    g2.draw(path);
+                    for(int y = minY; y <= maxY; y++){
+                        for(int x = minX; x<= maxX; x++){
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+                            if(b1 >= 0 && b2 >= 0 && b3 >= 0){
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+
+                                if(zBuffer[zIndex] < depth){
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
+                            }
+                        }
+                    }
+
+                    g2.drawImage(img, 0, 0, null);
+
+                    
                 }
             }
         };
